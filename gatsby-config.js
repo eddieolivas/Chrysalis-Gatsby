f878e02a5f6402d8f0f80bd05da425877e5b686c
eddieospaghettio <eddie@chrysaliswebdevelopment.com>
2021-05-02 16:09:18 -0700

    Set up gatsby-plugin-google-fonts to preload fonts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,6 +23,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Raleway`,
+          `300,400,500,700` // you can also specify font weights and styles
+        ],
+        display: 'swap'
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
